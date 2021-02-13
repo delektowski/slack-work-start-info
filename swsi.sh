1a330d1e0d84c12b5b4c5e6ff6e65d783504f9f8
Marcin Delektowski <marcin.delektowski@gmail.com>
2021-02-13 22:36:19 +0100

    Initial commit

---
+++ swsi.sh
@@ -0,0 +1,14 @@
+USER_NAME=$(grep USER_NAME .env | cut -d '=' -f2)
+CHANNEL=$(grep CHANNEL .env | cut -d '=' -f2)
+IMG_URL=$(grep IMG_URL .env | cut -d '=' -f2)
+SLACK_WEBHOOK=$(grep SLACK_WEBHOOK .env | cut -d '=' -f2)
+
+GET_TEXT(){
+  START_TIME=$(date +"%H:%M")
+  END_TIME=$(date -v+8H +"%H:%M" )
+  BREAK_TIME=$(date -v+4H +"%H:%M" )
+  TEXT="Hej! Dziś zaczynam od: $START_TIME, a kończę o: $END_TIME; Przerwa ok.: $BREAK_TIME \\n Cytat na dziś: 'kokos' "
+  echo "$TEXT"
+}
+
+curl -X POST --data-urlencode "payload={\"channel\": $CHANNEL, \"username\": $USER_NAME, \"text\": \"$(GET_TEXT)\", \"icon_url\": $IMG_URL}" "$SLACK_WEBHOOK"
